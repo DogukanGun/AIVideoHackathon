48239b8ccd8211d9b0149a00fc6ed73774a318ac
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-10-16 15:38:23 +0200

    add markeplace contract

---
+++ smart_contracts/Marketplace.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+import "./Trade.sol";
+
+contract Marketplace {
+
+    address aiProcessor;
+
+    mapping(address=>address) activeTrades;
+
+    constructor(address _aiProcessor) {
+        aiProcessor = _aiProcessor;
+    }
+
+    function startTrade(address _seller,address _buyer,uint256 _amount) public {
+        address tradeContractAddress = address(new Trade(_seller,_buyer,_amount,address(this)));
+        activeTrades[msg.sender] = tradeContractAddress;
+    }
+
+    function verifyTransfer(address smartContractAddress) public {
+        require(msg.sender == aiProcessor,"Only Ai processor can call this function");
+        ITrade(smartContractAddress).verifyTrade();
+    }
+
+}
---
+++ smart_contracts/Trade.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+interface ITrade {
+    function verifyTrade() payable external;
+    function missingAmount() view external returns(uint256);
+}
+
+
+contract Trade is ITrade {
+
+    address seller;
+    address buyer;
+    uint256 amount;
+    address marketplace;
+    bool isFirstPaymentDone;
+    bool isTradeVerified;
+
+
+    constructor(address _seller,address _buyer,uint256 _amount,address _marketplace){
+        seller = _seller;
+        buyer = _buyer;
+        amount = _amount;
+        marketplace = _marketplace;
+    }
+
+    receive() external payable {
+        if(msg.sender == buyer){
+            if(msg.value >= amount){
+                isFirstPaymentDone = true;
+            }else{
+                revert("Amount is not enough");
+            }
+        }else{
+            revert("Money must be sent by buyer");
+        }
+    }
+
+    function missingAmount() view external returns(uint256) {
+        if(address(this).balance > amount){
+            return 0;
+        }
+        return amount - address(this).balance;
+    }
+
+    function verifyTrade() payable external {
+        require(msg.sender == marketplace, "Only marketplace can verify this transfer");
+        isTradeVerified = true;
+        payable(seller).transfer(address(this).balance);
+    }
+
+}
